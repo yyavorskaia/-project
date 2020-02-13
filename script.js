ca7d182dd536411aeb1a46dbe1b35a918fe07162
yyavorskaia <yyavorskaia@gmail.com>
2020-02-13 13:42:46 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,51 @@
+'use strict';
+
+/*var BorderWidth = 1;
+let second = 2;
+const pi = 3.14;
+
+console.log(4/0);*/
+
+
+/*var cat = { // создаем объект
+  name: 'Кекс', // name — ключ, 'Кекс' — значение
+  age: 5 // Ключ — age, значение — 5
+};*/
+
+/*console.log(cat.name);
+console.log(cat["name"]);
+
+var usersByDay = [812, 1360, 657, 1247];
+console.log(usersByDay[1]); // Выведет 1360*/
+
+
+
+// alert( "Я JavaScript!" );
+
+// let answer = confirm("A you here?");
+// console.log(answer);
+
+/*let answer = prompt("Есть ли вам 18?");
+console.log(answer);*/
+
+/*let answer = prompt("Есть ли вам 18?", "Да"); //
+console.log(answer);*/
+
+
+// console.log('1' + 'j');
+
+// let answer = +prompt("Есть ли вам 18?");
+// console.log(typeof(answer));
+
+/*let incr = 10,
+    decr = 10;
+
+    console.log(++incr); //11
+    console.log(--decr); //9*/
+
+/*let counter = 0;
+counter++;
+
+alert(counter); // 2, обе строки сделали одно и то же*/
+
+console.log(5 % 2);
