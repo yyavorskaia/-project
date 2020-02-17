718f74e838eeb6b5fb804de71b55f14f4692cf89
yyavorskaia <yyavorskaia@gmail.com>
2020-02-17 23:58:48 +0300

    Revert "task_1"
    This reverts commit c02ffb635e6934b6cb03acf60607008e799e3df5.

--- script.js
+++ script.js
@@ -1,22 +1,51 @@
-let money = prompt("Ваш бюджет на месяц?", " " );
-let time = prompt("Введите дату в формате YYYY-MM-DD", " " );
+'use strict';
 
-let appData = {
-  moneyData: money,
-  timeData: time,
-  expenses: {},
-  optionalExpenses: {},
-  income: [],
-  savings: false
-};
+/*var BorderWidth = 1;
+let second = 2;
+const pi = 3.14;
 
-let article = prompt("Введите обязательную статью расходов в этом месяце", " " );
-let cost = prompt("Во сколько обойдется ?", " " );
+console.log(4/0);*/
 
-let article1 = prompt("Введите обязательную статью расходов в этом месяце", " " );
-let cost1 = prompt("Во сколько обойдется ?", " " );
 
-appData.expenses[article] = cost;
-appData.expenses[article1] = cost1;
+/*var cat = { // создаем объект
+  name: 'Кекс', // name — ключ, 'Кекс' — значение
+  age: 5 // Ключ — age, значение — 5
+};*/
 
-alert(appData.moneyData / 30);
+/*console.log(cat.name);
+console.log(cat["name"]);
+
+var usersByDay = [812, 1360, 657, 1247];
+console.log(usersByDay[1]); // Выведет 1360*/
+
+
+
+// alert( "Я JavaScript!" );
+
+// let answer = confirm("A you here?");
+// console.log(answer);
+
+/*let answer = prompt("Есть ли вам 18?");
+console.log(answer);*/
+
+/*let answer = prompt("Есть ли вам 18?", "Да"); //
+console.log(answer);*/
+
+
+// console.log('1' + 'j');
+
+// let answer = +prompt("Есть ли вам 18?");
+// console.log(typeof(answer));
+
+/*let incr = 10,
+    decr = 10;
+
+    console.log(++incr); //11
+    console.log(--decr); //9*/
+
+/*let counter = 0;
+counter++;
+
+alert(counter); // 2, обе строки сделали одно и то же*/
+
+console.log(5 % 2);
